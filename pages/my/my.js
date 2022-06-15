04dd618dab8a251478ebfb2417b9415ab5bb0924
username <956465331@qq.com>
2022-06-15 14:00:07 +0800

    增加我的页面和登陆校验

--- pages/my/my.js
+++ pages/my/my.js
@@ -1,12 +1,81 @@
 // pages/my/my.js
 Page({
+ 
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    myList:[
+      {id:1,title:"车辆设置",name:"falut_history",image:"/images/settings.png"},
+      {id:2,title:"报警设置",name:"fault_analysis",image:"/images/110.png"},
+      {id:3,title:"电话信息",name:"history_history",image:"/images/phone.png"},
+      {id:4,title:"关于我们",name:"history_history",image:"/images/about.png"},
+    ],
+    userInfo: {},
+    NotLoginMe:true,
+    flagA:true,
+    flagB:true
   },
+  //登陆方法
+  loginMe(){
+    wx.login({
+      timeout: 1000,
+      success:(res)=>{
+        if (res.code) {
+            //发起网络请求 成功则跳转
+            wx.request({
+              url: 'https://gallifrey.asia/TAPI',
+              data: {
+                code: res.code
+              },
+              success:(res)=>{
+                // console.log(res);
+                if(res.statusCode==200){
+                  this.setData({
+                    flagA:true
+                  })
+                  wx.showToast({
+                    title: '网络请求成功',
+                  })
+                }else{
+                  wx.showToast({
+                    icon:"error",
+                    title: '网络请求失败',
+                  })
+                }
+              }  
+
+            })
+        } else {
+            console.log('网络请求失败！' + res.errMsg)
+        }
+      }
+    })
+    wx.getUserProfile({
+      desc: '用于完善会员资料', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
+      success: (res) => {
+        this.setData({
+          userInfo: res.userInfo,
+          flagB:true
+        })
+        if(this.data.flagA==true&&this.data.flagB==true){
+          wx.showToast({
+            title: '登陆成功',
+          })
+          this.setData({
+            NotLoginMe:false
+          })
+        }else{
+          wx.showToast({
+            title: '登陆失败',
+          })
+        }
+      }
+    })
+  },
+  
+
 
   /**
    * 生命周期函数--监听页面加载
